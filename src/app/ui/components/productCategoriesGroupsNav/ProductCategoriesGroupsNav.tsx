b7afbf00d5940c98b99feaf99b0de14d25f61ec9
Artsiom Zhalkouski <azhalkouski@gmail.com>
2024-11-28 16:24:37 +0300

    implement ProductCategoriesGroupsNav's basic functionality

--- src/app/ui/components/productCategoriesGroupsNav/ProductCategoriesGroupsNav.tsx
+++ src/app/ui/components/productCategoriesGroupsNav/ProductCategoriesGroupsNav.tsx
@@ -1,15 +1,54 @@
 "use client"
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import styles from "../../../page.module.css";
+
+type customerType = "women" | "men";
+
+const categoriesGroupsLinks = {
+  women: [
+    {
+      href: "/women-clothing",
+      groupTitle: "Clothing"
+    },
+    {
+      href: "/women-shoes",
+      groupTitle: "Shoes"
+    },
+  ],
+  men: [
+    {
+      href: "/men-clothing",
+      groupTitle: "Clothing"
+    },
+    {
+      href: "/men-shoes",
+      groupTitle: "Shoes"
+    },
+  ]
+} as {[key in customerType]: {href: string, groupTitle: string}[]};
 
 function ProductCategoriesGroupsNav() {
-  const clothingHref = '/women-clothing';
-  const shoesHref = '/women-shoes';
+  const pathname = usePathname();
+
+  const customerTypeSegment = pathname.slice(1).split('-')[0] as customerType;
+  const links = categoriesGroupsLinks[customerTypeSegment];
+
+  if (links === undefined) {
+    // TODO: logger.error(`Unsupported route segment as customerTypeSegment - ${customerTypeSegment}`);
+    // ! Error boudaries? React suspense?
+    throw new Error(`Unsupported route segment as customerTypeSegment`);
+  }
 
   return (
-    <div style={{border: '1px solid green'}}>
-      <Link href={clothingHref}>Clothing</Link>
-      <Link href={shoesHref}>Shoes</Link>
+    <div className={styles.ctas}>
+      {links.map(({href, groupTitle}) => {
+        return (
+          <Link key={href} href={href} className={styles.secondary}>{groupTitle}</Link>
+        );
+      })}
     </div>
   );
 }
