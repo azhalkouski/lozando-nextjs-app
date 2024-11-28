5453f8609ef36f69833faceb7075cfd74ec4635f
Artsiom Zhalkouski <azhalkouski@gmail.com>
2024-11-28 12:46:04 +0300

    implement basic Links in ProductCategoriesGroupsNav

--- src/app/ui/components/productCategoriesGroupsNav/ProductCategoriesGroupsNav.tsx
+++ src/app/ui/components/productCategoriesGroupsNav/ProductCategoriesGroupsNav.tsx
@@ -1,8 +1,15 @@
+"use client"
+
+import Link from "next/link";
 
 function ProductCategoriesGroupsNav() {
+  const clothingHref = '/women-clothing';
+  const shoesHref = '/women-shoes';
+
   return (
-    <div style={{border: '1px solid red'}}>
-      ProductCategoriesGroupsNav
+    <div style={{border: '1px solid green'}}>
+      <Link href={clothingHref}>Clothing</Link>
+      <Link href={shoesHref}>Shoes</Link>
     </div>
   );
 }
